f7ffb9038e7f6cf7d04925031c01521c8770f9e4
riggi <simone.riggi@gmail.com>
2020-10-14 17:09:35 +0200

    Add test with custom decorator for require_login

--- caesar_rest/index_route.py
+++ caesar_rest/index_route.py
@@ -3,6 +3,11 @@
 ##############################
 from flask import current_app, Blueprint, render_template, request, redirect
 from caesar_rest import oidc
+from caesar_rest.decorators import custom_require_login
+
+# Get logger
+import logging
+logger = logging.getLogger(__name__)
 
 ##############################
 #   CREATE BLUEPRINT
@@ -11,24 +16,51 @@ index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/')
 def index():
-	if oidc.user_loggedin:
-		return render_template('index.html', username=oidc.user_getfield('email'))
-        	#return 'Welcome {email}'.format(email=oidc.user_getfield('email'))
-    	else:
-        	return 'Not logged in, <a href="/login">Log in</a> '
+
+	aai_enabled= current_app.config['USE_AAI']
+	has_oidc= (oidc is not None)
+	logger.info("use_aai? %d has_oidc? %d" % (aai_enabled, has_oidc))
+
+	if aai_enabled and has_oidc:
+		if oidc.user_loggedin:
+			return render_template('index.html', username=oidc.user_getfield('email'))
+			#return 'Welcome {email}'.format(email=oidc.user_getfield('email'))
+		else:
+			return 'Not logged in, <a href="/login">Log in</a> '
+	else:
+		return render_template('index.html')
 
 
 @index_bp.route('/login')
-@oidc.require_login
+#@oidc.require_login
+@custom_require_login
 def login():
-    user_info = oidc.user_getinfo(['preferred_username', 'email', 'sub', 'name'])
-    # return value is a dictionary with keys the above keywords
-    return redirect("/", code=302) #'Welcome {user_info}'.format(user_info=user_info['name'])
+
+	aai_enabled= current_app.config['USE_AAI']
+	has_oidc= (oidc is not None)
+	logger.info("use_aai? %d has_oidc? %d" % (aai_enabled, has_oidc))
+
+	if aai_enabled and has_oidc:
+		user_info = oidc.user_getinfo(['preferred_username', 'email', 'sub', 'name'])
+		# return value is a dictionary with keys the above keywords
+		return redirect("/", code=302) #'Welcome {user_info}'.format(user_info=user_info['name'])
+
+	else:
+		return render_template('index.html')
+
 
 
 @index_bp.route('/logout')
 def logout():
-    """Performs local logout by removing the session cookie."""
+	"""Performs local logout by removing the session cookie."""
 
-    oidc.logout()
-    return 'Hi, you have been logged out! <a href="/">Return</a>'
+	aai_enabled= current_app.config['USE_AAI']
+	has_oidc= (oidc is not None)
+	logger.info("use_aai? %d has_oidc? %d" % (aai_enabled, has_oidc))
+
+	if aai_enabled and has_oidc:
+		oidc.logout()
+		return 'Hi, you have been logged out! <a href="/">Return</a>'
+	else:
+		return 'Bye bye'
+
