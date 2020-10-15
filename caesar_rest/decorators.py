ac913720d8dc42430e7c1ecb17ac4797a1d85811
riggi <simone.riggi@gmail.com>
2020-10-15 12:50:25 +0200

    Add login/logout info with oidc custom decorator (to be checked)

---
+++ caesar_rest/decorators.py
@@ -0,0 +1,32 @@
+##############################
+#   MODULE IMPORTS
+##############################
+from functools import wraps
+from flask import current_app, request, g
+from caesar_rest import oidc
+
+# Get logger
+import logging
+logger = logging.getLogger(__name__)
+
+def custom_require_login(func):
+	""" Decorator to require login only if AAI is enabled """
+
+	@wraps(func)
+	def decorated(*args, **kwargs):
+		aai_enabled= current_app.config['USE_AAI']
+		has_oidc= (oidc is not None)
+		logger.info("use_aai? %d has_oidc? %d" % (aai_enabled, has_oidc))
+
+		if aai_enabled and has_oidc:
+			if g.oidc_id_token is None:
+				logger.info("Calling OIDC login ...")
+				#return oidc.require_login(func(*args, **kwargs))
+				#return oidc.require_login(func)
+				#return oidc.redirect_to_auth_server(func, request.values)
+				return oidc.redirect_to_auth_server(request.url)
+			
+		else:		
+			return func(*args, **kwargs)
+
+	return decorated
--- caesar_rest/index_route.py
+++ caesar_rest/index_route.py
@@ -2,7 +2,12 @@
 #   MODULE IMPORTS
 ##############################
 from flask import current_app, Blueprint, render_template, request, redirect
+from caesar_rest import oidc
+from caesar_rest.decorators import custom_require_login
 
+# Get logger
+import logging
+logger = logging.getLogger(__name__)
 
 ##############################
 #   CREATE BLUEPRINT
@@ -11,4 +16,52 @@ index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/')
 def index():
-	return render_template('index.html')
+	#return render_template('index.html')
+
+	aai_enabled= current_app.config['USE_AAI']
+	has_oidc= (oidc is not None)
+	logger.info("use_aai? %d has_oidc? %d" % (aai_enabled, has_oidc))
+
+	if aai_enabled and has_oidc:
+		if oidc.user_loggedin:
+			return render_template('index.html', username=oidc.user_getfield('email'))
+			#return 'Welcome {email}'.format(email=oidc.user_getfield('email'))
+		else:
+			return 'Not logged in, <a href="/login">Log in</a> '
+	else:
+		return render_template('index.html')
+
+
+@index_bp.route('/login')
+#@oidc.require_login
+@custom_require_login
+def login():
+
+	aai_enabled= current_app.config['USE_AAI']
+	has_oidc= (oidc is not None)
+	logger.info("use_aai? %d has_oidc? %d" % (aai_enabled, has_oidc))
+
+	if aai_enabled and has_oidc:
+		user_info = oidc.user_getinfo(['preferred_username', 'email', 'sub', 'name'])
+		# return value is a dictionary with keys the above keywords
+		return redirect("/", code=302) #'Welcome {user_info}'.format(user_info=user_info['name'])
+
+	else:
+		return render_template('index.html')
+
+
+
+@index_bp.route('/logout')
+def logout():
+	"""Performs local logout by removing the session cookie."""
+
+	aai_enabled= current_app.config['USE_AAI']
+	has_oidc= (oidc is not None)
+	logger.info("use_aai? %d has_oidc? %d" % (aai_enabled, has_oidc))
+
+	if aai_enabled and has_oidc:
+		oidc.logout()
+		return 'Hi, you have been logged out! <a href="/">Return</a>'
+	else:
+		return 'Bye bye'
+
