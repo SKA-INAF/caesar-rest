9517bbfb3c9a2cb588e922fa986dbe73792a14cf
riggi <simone.riggi@gmail.com>
2020-10-26 11:47:17 +0100

    Add task registration in mongo and endpoint to retrieve all job ids

--- caesar_rest/job_route.py
+++ caesar_rest/job_route.py
@@ -36,7 +36,7 @@ from caesar_rest import workers
 from caesar_rest.workers import background_task
 from caesar_rest import oidc
 from caesar_rest.decorators import custom_require_login
-
+from caesar_rest import mongo
 
 # Get logger
 logger = logging.getLogger(__name__)
@@ -51,7 +51,9 @@ job_output_bp = Blueprint('job_output', __name__,url_prefix='/caesar/api/v1.0')
 job_cancel_bp = Blueprint('job_cancel', __name__,url_prefix='/caesar/api/v1.0')
 
 
-
+#=================================
+#===      JOB SUBMIT 
+#=================================
 @job_bp.route('/job', methods=['POST'])
 @custom_require_login
 def submit_job():
@@ -70,6 +72,11 @@ def submit_job():
 	username= 'anonymous'
 	if aai_enabled and has_oidc:
 		username= oidc.user_getfield('preferred_username')
+
+	# - Get mongo info
+	mongo_enabled= current_app.config['USE_MONGO']
+	has_mongo= (mongo is not None)
+	use_mongo= (mongo_enabled and has_mongo)
 
 	# - Get request data
 	req_data = request.get_json(silent=True)
@@ -122,6 +129,35 @@ def submit_job():
 	job_id= task.id
 	logger.info("Submitted job with id=%s ..." % job_id)
 
+	# - Register task id in mongo
+	if use_mongo:
+		logger.info("Creating job object for task %s ..." % job_id)
+		job_obj= {
+			"job_id": job_id,
+			"submit_date": submit_date,
+			"app": app_name,	
+			"job_inputs": job_inputs,
+			"metadata": '', # FIX ME
+			"tag": ''	# FIX ME
+		}
+
+		collection_name= username + '.jobs'
+
+		try:
+			logger.info("Creating or retrieving job collection for user %s ..." % username)
+			job_collection= mongo.db[collection_name]
+
+			logger.info("Adding job obj to collection ...")
+			item_id= data_collection.insert(data_fileobj)
+		
+		except:
+			logger.warn("Failed to create and register job in DB!")
+			flash('Job submitted but failed to be registered in DB!')
+			logger.warn("Job %s submitted but failed to be registered in DB!" % job_id)
+			res['status']= 'Job submitted but failed to be registered in DB'
+			return make_response(jsonify(res),500)
+
+
 	# - Fill response
 	res['job_id']= job_id
 	res['submit_date']= submit_date
@@ -132,8 +168,47 @@ def submit_job():
 	return make_response(jsonify(res),202)
 
 
+#=================================
+#===      JOB IDs 
+#=================================
+@job_bp.route('/jobs', methods=['GET'])
+@custom_require_login
+def get_job_ids():
+	""" Retrieve all job ids per user """
 
+	# - Get aai info
+	aai_enabled= current_app.config['USE_AAI']
+	has_oidc= (oidc is not None)
+	username= 'anonymous'
+	if aai_enabled and has_oidc:
+		username= oidc.user_getfield('preferred_username')
 
+	# - Get mongo info
+	mongo_enabled= current_app.config['USE_MONGO']
+	has_mongo= (mongo is not None)
+	use_mongo= (mongo_enabled and has_mongo)
+
+	# - Get all job ids.
+	#   NB: Only supported with Mongo backend
+	d= {}	
+	collection_name= username + '.jobs'
+	if use_mongo:
+		job_collection= mongo.db[collection_name]
+		job_ids= job_collection.find().distinct('job_id')
+		job_id_list= [str(jobid) for jobid in job_ids]
+		d.update({'job_ids':job_id_list})
+
+	else:
+		d['status']= 'Server is running without MongoDB backend, so this functionality is not supported'
+		d['job_ids']= []
+		return make_response(jsonify(d),404)
+		
+	return make_response(jsonify(d),200)
+	
+
+#=================================
+#===      JOB CANCEL 
+#=================================
 @job_status_bp.route('/job/<task_id>/cancel',methods=['GET','POST'])
 @custom_require_login
 def cancel_job(task_id):
@@ -166,6 +241,9 @@ def cancel_job(task_id):
 	return make_response(jsonify(res),200)
 
 
+#=================================
+#===      JOB STATUS HELPER
+#=================================
 def compute_job_status(task_id):
 	""" Compute job status """
 
@@ -219,6 +297,9 @@ def compute_job_status(task_id):
 	return res
 
 
+#=================================
+#===      JOB STATUS 
+#=================================
 @job_status_bp.route('/job/<task_id>/status',methods=['GET'])
 @custom_require_login
 def get_job_status(task_id):
@@ -233,6 +314,29 @@ def get_job_status(task_id):
 	res['exit_status']= ''
 	res['elapsed_time']= ''
 
+	# - Get aai info
+	aai_enabled= current_app.config['USE_AAI']
+	has_oidc= (oidc is not None)
+	username= 'anonymous'
+	if aai_enabled and has_oidc:
+		username= oidc.user_getfield('preferred_username')
+
+	# - Get mongo info
+	mongo_enabled= current_app.config['USE_MONGO']
+	has_mongo= (mongo is not None)
+	use_mongo= (mongo_enabled and has_mongo)
+
+	# - Search job id in user collection
+	collection_name= username + '.jobs'
+	if use_mongo:
+		job_collection= mongo.db[collection_name]
+		job= job_collection.find_one({'job_id': str(task_id)})
+		if not job or job is None:
+			errmsg= 'Job ' + task_id + ' not found for user ' + username + '!'
+			logger.warn(errmsg)
+			res['status']= errmsg
+			return make_response(jsonify(res),404)
+
 	# - Retrieve job status
 	try:
 		res= compute_job_status(task_id)
@@ -243,35 +347,9 @@ def get_job_status(task_id):
 		return make_response(jsonify(res),404)
 	
 
-	# - Get task 
-	#task = background_task.AsyncResult(task_id)
-	#if not task:
-	#	res['status']= 'No task found with given id'
-	#	return make_response(jsonify(res),404)
-	
-	# - Check if task ID not existing 
-	#   NB: Celery does not throw exceptions in case task id is not known, it just set task state to PENDING, so try to handle this...wtf!
-	#if task.state=='PENDING' and (task.result==None or task.info==None): 
-	#	res['status']= 'No task found with id ' + task_id + '!'
-	#	return make_response(jsonify(res),404)
-		
-	# - Celery set to SUCCESS whenever task return a value, even if update_state is set to FAILURE before return or if returning Ignore() or a <0 code so need to handle this case ... wtf!
-	#task_state= task.state 
-	#task_exit= task.info.get('exit_code', '')
-	#task_status= task.info.get('status', '')
-	#task_elapsed= task.info.get('elapsed_time', '')
-	#if task_state=='SUCCESS' and task_exit!='0':
-	#	task_state= 'FAILURE'
-	
-	# - Fill task info
-	#res['state']= task_state
-	#res['status']= task_status
-	#res['exit_status']= task_exit
-	#res['elapsed_time']= task_elapsed
-	
-	#return make_response(jsonify(res),200)
-
-
+#=================================
+#===      JOB OUTPUT 
+#=================================
 @job_output_bp.route('/job/<task_id>/output',methods=['GET'])
 @custom_require_login
 def get_job_output(task_id):
