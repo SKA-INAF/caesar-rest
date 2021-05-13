853cd003449b38b56df8ac1ffc1bbc343a5dff37
riggi <simone.riggi@gmail.com>
2021-05-13 19:33:30 +0200

    Do not cancel job if already completed or failed or timed out

--- caesar_rest/job_route.py
+++ caesar_rest/job_route.py
@@ -517,7 +517,7 @@ def cancel_job(task_id):
 	job_state= job["state"]
 
 	# - Do not cancel job if already completed or failed
-	if job_state=="SUCCESS" or job_state="FAILURE":
+	if job_state=="SUCCESS" or job_state="FAILURE" or job_state="TIMED-OUT":
 		msg= 'Job ' + task_id + ' already completed with state ' + job_state + ', nothing to be canceled.'
 		logger.info(msg)
 		res['status']= msg
