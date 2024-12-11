dd65076f2ecd80d627a5364b8f0b3217748b87db
Simone Riggi <simone.riggi@gmail.com>
2024-12-11 18:43:49 +0100

    Remove dataset name from route

--- caesar_rest/job_route.py
+++ caesar_rest/job_route.py
@@ -76,8 +76,7 @@ def get_dataset_names():
 		if value["path"]=="":
 			continue
 		dataset_names[key]= value
-		if "path" in dataset_names[key]:
-			del dataset_names[key]["path"]
+		del dataset_names[key]["path"]
 	
 	return make_response(jsonify(dataset_names), 200)
 
